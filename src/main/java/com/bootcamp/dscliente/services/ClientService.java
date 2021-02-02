39aeabcec2e667fa5b0a1e58a89db9740e35b6be
aaaujm <juniorrafael95@yahoo.com>
2021-02-01 21:15:22 -0300

    HTTP PUT and DELETE methods developed

--- src/main/java/com/bootcamp/dscliente/services/ClientService.java
+++ src/main/java/com/bootcamp/dscliente/services/ClientService.java
@@ -2,7 +2,11 @@ package com.bootcamp.dscliente.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -11,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.bootcamp.dscliente.dtos.ClientDTO;
 import com.bootcamp.dscliente.entities.Client;
 import com.bootcamp.dscliente.entities.repositories.ClientRepository;
+import com.bootcamp.dscliente.services.exceptions.DatabaseException;
 import com.bootcamp.dscliente.services.exceptions.ResourceNotFoundException;
 
 
@@ -38,6 +43,33 @@ public class ClientService {
 		copyDtoToEntity(dto, entity);
 		entity = repository.save(entity);
 		return new ClientDTO(entity);
+	}
+	
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		
+		try {
+			Client entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not Found "+ id);
+		}
+		
+	}
+	
+	
+	public void delete(Long id) {
+		
+		try {
+		repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e){
+			throw new ResourceNotFoundException("Id not Found "+ id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity Violation");
+		}
 	}
 
 	private void copyDtoToEntity(ClientDTO dto, Client entity) {
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,13 @@
-INSERT INTO tb_client (name, cpf, income, birth_Date,children) VALUES ('Rafael Junior Martins Avelino', '26456523115',3986.5,NOW(),2);
-INSERT INTO tb_client (name, cpf, income, birth_Date,children) VALUES ('Gabriel Henrique Martins Avelino', '05421220122',5000,NOW(),1);
-INSERT INTO tb_client (name, cpf, income, birth_Date,children) VALUES ('Lorraine Araujo Cristina Cota', '13302512368',3980.3,NOW(),3);
-INSERT INTO tb_client (name, cpf, income, birth_Date,children) VALUES ('Jose da Conceição Avelino', '02365895522',5000,NOW(),1);
-INSERT INTO tb_client (name, cpf, income, birth_Date,children) VALUES ('Rafael Junior Martins Avelino', '26456523115',3986.5,NOW(),2);
-INSERT INTO tb_client (name, cpf, income, birth_Date,children) VALUES ('Gabriel Henrique Martins Avelino', '05421220122',5000,NOW(),1);
-INSERT INTO tb_client (name, cpf, income, birth_Date,children) VALUES ('Lorraine Araujo Cristina Cota', '13302512368',3980.3,NOW(),3);
-INSERT INTO tb_client (name, cpf, income, birth_Date,children) VALUES ('Jose da Conceição Avelino', '02365895522',5000,NOW(),1);
-INSERT INTO tb_client (name, cpf, income, birth_Date,children) VALUES ('Rafael Junior Martins Avelino', '26456523115',3986.5,NOW(),2);
-INSERT INTO tb_client (name, cpf, income, birth_Date,children) VALUES ('Gabriel Henrique Martins Avelino', '05421220122',5000,NOW(),1);
-INSERT INTO tb_client (name, cpf, income, birth_Date,children) VALUES ('Lorraine Araujo Cristina Cota', '13302512368',3980.3,NOW(),3);
-INSERT INTO tb_client (name, cpf, income, birth_Date,children) VALUES ('Jose da Conceição Avelino', '02365895522',5000,NOW(),1);
+INSERT INTO tb_client (name, cpf, income, birth_Date,children) VALUES ('Rafael Junior Martins Avelino', '26456523115',3986.5,'1995-09-18T10:30:00Z',2);
+INSERT INTO tb_client (name, cpf, income, birth_Date,children) VALUES ('Gabriel Henrique Martins Avelino', '05421220122',5000,'1991-05-18T10:30:00Z',1);
+INSERT INTO tb_client (name, cpf, income, birth_Date,children) VALUES ('Lorraine Araujo Cristina Cota', '13302512368',3980.3,'1993-05-13T10:30:00Z',3);
+INSERT INTO tb_client (name, cpf, income, birth_Date,children) VALUES ('Jose da Conceição Avelino', '02365895522',5000,'1978-01-15T10:30:00Z',1);
+INSERT INTO tb_client (name, cpf, income, birth_Date,children) VALUES ('Rosiane Aparecida Martins Coelho', '26456523115',3986.5,'1990-05-17T10:02:00Z',2);
+INSERT INTO tb_client (name, cpf, income, birth_Date,children) VALUES ('Silane Aparecida', '05421220122',5000,'1993-05-15T10:30:00Z',1);
+INSERT INTO tb_client (name, cpf, income, birth_Date,children) VALUES ('Renê Aparecida', '13302512368',3980.3,'1999-02-18T10:30:00Z',3);
+INSERT INTO tb_client (name, cpf, income, birth_Date,children) VALUES ('Thulio Pereira Costa', '02365895522',5000,'1995-05-15T10:17:00Z',1);
+INSERT INTO tb_client (name, cpf, income, birth_Date,children) VALUES ('Márcio Gomes Francisco', '26456523115',3986.5,'1989-03-12T10:30:00Z',2);
+INSERT INTO tb_client (name, cpf, income, birth_Date,children) VALUES ('Gabriel Melo Da Silva', '05421220122',5000,'1990-05-19T10:15:00Z',1);
+INSERT INTO tb_client (name, cpf, income, birth_Date,children) VALUES ('Luiz Gustavo Grilo', '13302512368',3980.3,'1968-05-18T10:30:00Z',3);
+INSERT INTO tb_client (name, cpf, income, birth_Date,children) VALUES ('Igor Roberto', '02365895522',5000,'1990-12-18T10:30:00Z',1);
 
